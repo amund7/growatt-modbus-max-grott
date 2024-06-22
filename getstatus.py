124a38ac9b7dcd6c9a4814a24992c6489027c300
Amund Borsand <amundster@gmail.com>
2024-06-22 14:18:34 +0200

    First commit

--- getstatus.py
+++ getstatus.py
@@ -21,7 +21,6 @@ influxmeasurement = "inverter"
 
 broker = '192.168.0.111'
 port = 1883
-#topic = "python/mqtt"
 topic = "energy/growatt"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 username = 'mqtt'
@@ -68,15 +67,6 @@ class Growatt:
         self.client = client
         self.name = name
         self.unit = unit
-
-        #row = self.client.read_holding_registers(73, unit=self.unit)
-        #if type(row) is ModbusIOException:
-        #    if gwverbose: print("GWVERBOSE1",row)
-        #    raise row
-        #self.modbusVersion = row.registers[0]
-
-        #row = self.client.write_register(250, 1)
-        #print(row)
 
     def read(self):
         row = self.client.read_input_registers(0, 116, unit=self.unit)
@@ -217,102 +207,7 @@ class Growatt:
               #"reactive_pwr_tot": row.registers[236]*65536+row.registers[237]
             }
         }
-#        info = {                                    # ==================================================================
-#            "Module": unit,
-#            "StatusCode": row.registers[0],         # N/A,      Inverter Status,    Inverter run state
-#            "Status": StatusCodes[row.registers[0]],
-#            "PpvH":     float(row.registers[1]) / 10,              # 0.1W,     PV1 Charge power (high)
-#            "PpvL":     float(row.registers[2]) / 10,              # 0.1W,     PV1 Charge power (low) 
-#            "Vpv1":     float(row.registers[3]) / 10,               # 0.1V,     PV1 voltage
-#            "PV1Curr":  float(row.registers[4]) / 10,          # 0.1A,     Buck1 current
-#            "Ppv1H":    float(row.registers[5]) / 10,              # 0.1W,     PV1 Charge power (high)
-#            "Ppv1L":    float(row.registers[6]) / 10,              # 0.1W,     PV1 Charge power (low) 
-#            "Vpv2":     float(row.registers[7]) / 10,               # 0.1V,     PV2 voltage
-#            "PV2Curr":  float(row.registers[8]) / 10,          # 0.1A,     Buck1 current
-#            "Ppv2H":    float(row.registers[9]) / 10,              # 0.1W,     PV2 Charge power (high)
-#            "Ppv2L":    float(row.registers[10]) / 10,              # 0.1W,     PV2 Charge power (low)
-#            "Vpv3":     float(row.registers[11]) / 10,               # 0.1V,     PV2 voltage
-#            "PV3Curr":  float(row.registers[12]) / 10,          # 0.1A,     Buck1 current
-#            "Ppv3H":    float(row.registers[13]) / 10,              # 0.1W,     PV2 Charge power (high)
-#            "Ppv3L":    float(row.registers[14]) / 10,              # 0.1W,     PV2 Charge power (low)
-#           
-#            "Buck1Curr": float(row.registers[7]) / 10,          # 0.1A,     Buck1 current
-#            "Buck2Curr": float(row.registers[8]) / 10,          # 0.1A,     Buck2 current
-#            "OP_WattH": float(row.registers[9]) / 10,           # 0.1W,     Output active power (high)
-#            "OP_WattL": float(row.registers[10]) / 10,          # 0.1W,     Output active power (low)
-#            "OP_VAH": float(row.registers[11]) / 10,            # 0.1VA     Output apparent power (high)
-#            "OP_VAL": float(row.registers[12]) / 10,            #
-#            "ACChr_WattH": float(row.registers[13]) / 10,       # 0.1W,     AC Charge Watts (high)
-#            "ACChr_WattL": float(row.registers[14]) / 10,       #
-#            "ACChr_VAH": float(row.registers[15]) / 10,         # 0.1VA,    AC Charge apparent power (high)
-#            "ACChr_VAL": float(row.registers[16]) / 10,         #
-#            "Bat_Volt": float(row.registers[17]) / 100,         # 0.01V,    Battery Voltage
-#            "BatterySOC": float(row.registers[18]) / 1,         # 1%,       Battery State of Charge
-#            "BusVolt": float(row.registers[19]) / 10,           # 0.1V,     Bus Voltage
-#            "GridVolt": float(row.registers[20]) / 10,          # 0.1V,     AC input Voltage
-#            "LineFreq": float(row.registers[21]) / 100,         # 0.01Hz,   AC input Freq
-#            "OutputVolt": float(row.registers[22]) / 10,        # 0.1V,     AC Output Voltage
-#            "OutputFreq": float(row.registers[23]) / 100,       # 0.01Hz    AC Output Freq
-#            "OutputDCV": float(row.registers[24]) / 10,         # 0.1V      DC Output Voltage
-#            "InvTemp": float(row.registers[25]) / 10,           # 0.1C      Inverter Temp
-#            "DCDCTemp": float(row.registers[26]) / 10,          # 0.1C      DCDC Temp
-#            "LoadPercent": float(row.registers[27]) / 10,       # 0.1%      Inverter Load Percent
-#            "Bat_dspp_V": float(row.registers[28]) / 100,         # 0.01V     Battery-port volt (DSP)
-#            "Bat_dspb_V": float(row.registers[29]) / 100,         # 0.01V     Battery-bus voltage (DSP)
-#            "TimeTotalH": float(row.registers[30]) / 2,         # 0.5S,     Time total H,       Work time total (high)
-#            "TimeTotalL": float(row.registers[31]) / 2,         # 0.5S,     Time total L,       Work time total (low)
-#            "Buck1Temp": float(row.registers[32]) / 10,         # 0.1C,     Temperature,        Inverter temperature
-#            "Buck2Temp": float(row.registers[33]) / 10,         # 0.1C,     Temperature,        Inverter temperature
-#            "OP_Curr": float(row.registers[34]) / 10,           # 0.1A,     Output Current
-#            "Inv_Curr": float(row.registers[35]) / 10,          # 0.1A,     Inv Current
-#            "AC_InWattH": float(row.registers[36]) / 10,        # 0.1W,     AC Input watt (high)
-#            "AC_InWattL": float(row.registers[37]) / 10,        # 0.1W,     AC Input watt (low)
-#            "AC_InVAH": float(row.registers[38]) / 10,          # 0.1A,     AC Input VA (high)
-#            "AC_InVAL": float(row.registers[39]) / 10,          # 0.1A,     AC Input VA (low)
-#            "Faultbit": float(row.registers[40]),               # &*1
-#            "Warnbit": float(row.registers[41]),                # &*1
-#            "Faultvalue": float(row.registers[42]),             # fault value
-#            "Warnvalue": float(row.registers[43]),              # warn value
-#            "DTC": float(row.registers[44]),                    #
-#            "CheckStep": float(row.registers[45]),              #
-#            "ProductionLM": float(row.registers[46]),           #
-#            "ConstPOKF": float(row.registers[47]),                # Constant power ok flag (0 no, 1 OK)
-#            "Epv1_todayH": float(row.registers[48]) / 10,       # 0.1kWh,   Energy today H,     Today generate energy (high)
-#            "Epv1_todayL": float(row.registers[49]) / 10,       # 0.1kWh,   Energy today l,     Today generate energy (low)
-#            "Epv1_totalH": float(row.registers[50]) / 10,       # 0.1kWh,   Energy total H,     generate energy total (high)
-#            "Epv1_totalL": float(row.registers[51]) / 10,       # 0.1kWh,   Energy total l,     generate energy total (low)
-#            "Epv2_todayH": float(row.registers[52]) / 10,       # 0.1kWh,   Energy today H,     Today generate energy (high)
-#            "Epv2_todayL": float(row.registers[53]) / 10,       # 0.1kWh,   Energy today l,     Today generate energy (low)
-#            "Epv2_totalH": float(row.registers[54]) / 10,       # 0.1kWh,   Energy total H,     generate energy total (high)
-#            "Epv2_totalL": float(row.registers[55]) / 10,       # 0.1kWh,   Energy total l,     generate energy total (low)
-#            "Eac_chrtodayH": float(row.registers[56]) / 10,     # 0.1kWh,   AC charge Energy Today (high)
-#            "Eac_chrtodayL": float(row.registers[57]) / 10,     # 0.1kWh,   AC charge Energy Todat (low)
-#            "Eac_chrtotalH": float(row.registers[58]) / 10,     # 0.1kWh,   AC charge Energy Total (high)
-#            "Eac_chrtotalL": float(row.registers[59]) / 10,     # 0.1kWh,   AC charge Energy Total (low)
-#            "Ebat_chrtodayH": float(row.registers[60]) / 10,    # 0.1kWh,   Bat discharge Energy Today (high)
-#            "Ebat_chrtodayL": float(row.registers[61]) / 10,    # 0.1kWh,   Bat discharge Energy Todat (low)
-#            "Ebat_chrtotalH": float(row.registers[62]) / 10,    # 0.1kWh,   Bat discharge Energy Total (high)
-#            "Ebat_chrtotalL": float(row.registers[63]) / 10,    # 0.1kWh,   Bat discharge Energy Total (low)
-#            "Eac_dischrtodayH": float(row.registers[64]) / 10,  # 0.1kWh,   AC discharge Energy Today (high)
-#            "Eac_dischrtodayL": float(row.registers[65]) / 10,  # 0.1kWh,   AC discharge Energy Todat (low)
-#            "Eac_dischrtotalH": float(row.registers[66]) / 10,  # 0.1kWh,   AC discharge Energy Total (high)
-#            "Eac_dischrtotalL": float(row.registers[67]) / 10,  # 0.1kWh,   AC discharge Energy Total (low)
-#            "Acchrcurr": float(row.registers[68]) / 10,         # 0.1A,     AC Charge Battery Current
-#            "AC_dischrwattH": float(row.registers[69]) / 10,    # 0.1W,     AC discharge watt (high)
-#            "AC_dischrwattL": float(row.registers[70]) / 10,    # 0.1W,     AC discharge watt (low)
-#            "AC_dischrvaH": float(row.registers[71]) / 10,      # 0.1VA     AC discharge va (high)
-#            "AC_dischrvaL": float(row.registers[72]) / 10,      # 0.1VA     AC discharge va (low)
-#            "Bat_dischrwattH": float(row.registers[73]) / 10,   # 0.1W      Bat discharge watts (high)
-#            "Bat_dischrwattL": float(row.registers[74]) / 10,   # 0.1W      Bat discharge watts (low)
-#            "Bat_dischrvaH": float(row.registers[75]) / 10,     # 0.1VA     Bat discharge va (high)
-#            "Bat_dischrvaL": float(row.registers[76]) / 10,     # 0.1VA     Bat discharge va (low)
-#            "Bat_wattH": float(row.registers[77]) / 10,         # 0.1W      Signed int positive discharge, negative battery charge power
-#            "Bat_wattL": float(row.registers[78]) / 10,         # 0.1W      Signed int positive discharge, negative battery charge power
-#            "Batovercharge": float(row.registers[80]),          # 0 no, 1 yes
-#            "Mpptfanspeed": float(row.registers[81]),           # 1%        Fan speed of MPPT Charger
-#            "Invfanspeed": float(row.registers[82]),            # 1%        Fan speed of Inverter
-#            
-#        }
+
         if gwinfodump: print(info, sep = "\n")
         return info
 
@@ -363,12 +258,7 @@ def on_message(client, userdata, msg):
     message=msg.payload
 
 def connect_mqtt():
-    #def on_connect(client, userdata, flags, rc):
-    #    if rc == 0:
-    #        print("Connected to MQTT Broker!")
-    #    else:
-    #        print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
+     # Set Connecting Client ID
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
 
@@ -383,9 +273,6 @@ def connect_mqtt():
     # Start the listening loop in a separate thread
     client.loop_start()
 
-    # Start a thread for the periodic publishing
-    #publish_thread = threading.Thread(target=publish_message, args=(client,))
-    #publish_thread.start()
     return client
 
 def publish(client, msg):
@@ -401,28 +288,6 @@ def publish(client, msg):
             print(f"Send msg to topic `{topic}`")
     else:
         print(f"Failed to send message to topic {topic}")
-
-
-# Do some shit
-
-#print("Establishing connection to Influx..", end="")
-#try:
-#  #influx = InfluxDBClient(host=influxhost, port=influxport,token="rE6f1VQoVWtx8MQEegoj51_StPbMfT6OAc-nXtOtk3LRSHUoSKojwe9KKsJzOHj-23EBp-fqHTDRvh_3-2vuZA==",org="a2d377bfd4b8c768")
-#  influx = InfluxDBClient(host=influxhost, port=influxport,username=influxuser,password=influxpass,database=influxdbname)
-#except:
-#  print("Failed")
-#  exit()
-#else:
-#  print("Done!")
-
-#print("Creating Influx Database ",influxdbname," ..", end="")
-#try:
-#    influx.create_database(influxdbname)
-#except:
-#  print("Failed")
-#  exit()
-#else:
-#  print("Done!")
 
 mqttclient = connect_mqtt()
 
@@ -443,10 +308,7 @@ else:
 print("Loading inverters.. ", end="")
 inverters = []
 for i in range(numinverters):
-  #unit is this concept in modbus of an address of the thing you are talking to on the bus
-  #it should be 1 for gw1, 2 for gw2, etc..etc  be sure to set any addressable things on the bus
-  #to a different unit number
-  #it looks like growatt it 
+
   unit=i+1
   name = "Growatt"+str(unit)
   measurement=influxmeasurement+str(unit)
